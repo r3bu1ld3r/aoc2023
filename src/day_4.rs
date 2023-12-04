59b79cadcbf43dcbe69ba7f171e3599246ba0f48
Anton V <61997468+r3bu1ld3r@users.noreply.github.com>
2023-12-05 00:33:43 +0400

    day 4 part 2

--- src/day_4.rs
+++ src/day_4.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 fn main() {
     let input = include_str!("../input/4.txt");
-    let ans = input
+    let cards = input
         .lines()
         .map(|line| line.split_once(':').unwrap().1)
         .map(|line| line.split_once('|').unwrap())
@@ -19,7 +19,28 @@ fn main() {
             )
         })
         .map(|(win_set, card)| card.iter().filter(|n| win_set.contains(n)).count())
-        .filter(|&n| n > 0)
-        .fold(0, |acc, n| acc + 2u32.pow((n - 1) as u32));
+        .collect::<Vec<_>>();
+
+    let mut i = 0;
+    let mut copies: Vec<u128> = vec![1; cards.len()];
+    while i < cards.len() {
+        if cards[i] == 0 {
+            i += 1;
+            continue;
+        };
+        let left_bound = i + 1;
+        let right_bound = if i + 1 + cards[i] < cards.len() {
+            i + 1 + cards[i]
+        } else {
+            cards.len()
+        };
+        
+        for j in left_bound..right_bound {
+            copies[j] += copies[i]
+        }
+        i += 1;
+    }
+
+    let ans = copies.iter().sum::<u128>();
     println!("{ans}");
 }
